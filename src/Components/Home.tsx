cd081f04c7808c42248b28d3a621dd7233154640
Jackson Casey <65034067+jaxcksn@users.noreply.github.com>
2022-04-22 16:33:41 -0500

    Added React Navigation

---
+++ src/Components/Home.tsx
@@ -0,0 +1,14 @@
+/* ------------------------------ React Imports ----------------------------- */
+import React from 'react';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello World!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
